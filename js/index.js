2a67e1305575c3b28b46409452859b71e77fa0d0
axg171231 <38120127+axg171231@users.noreply.github.com>
2018-07-31 22:01:22 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$("#doughnutChart").drawDoughnutChart([
+ { 
+    title: "Rent",
+    value : 800,  
+    color: "#76FF54" 
+  },
+  { 
+    title: "Transportation",
+    value : 130,  
+    color: "#F95D5D" 
+  },
+  { 
+    title: "Groceries",
+    value : 200,  
+    color: "#f3e32b" 
+  },
+  { 
+    title: "Clothing",
+    value : 70,  
+    color: "#06FFEF" 
+  },
+  
+  { 
+    title: "Bills and utilities",
+    value:  150,   
+    color: "#35a8ff" 
+  }
+]);
